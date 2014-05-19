23625cd9db0e729d445f8d33c181c5f42a5cf648
32graham <graham.josh@gmail.com>
2014-05-18 22:02:37 -0500

    add test project

---
+++ CustomerManagerTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomerManagerTest
+{
+    using System;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using CustomerManager.Services;
+
+    [TestClass]
+    public class CustomerServiceTests
+    {
+        [TestMethod]
+        public void TestList()
+        {
+            var customerService = new CustomerService();
+            var customers = customerService.List();
+
+            Assert.IsTrue(customers.Count() > 0);
+        }
+    }
+}
